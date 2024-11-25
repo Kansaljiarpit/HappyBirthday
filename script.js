e10d1fb875069d6643283622412eae953667395d
Kansaljiarpit <kansalarpit56@gmail.com>
2024-11-25 16:33:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeMessage() {
+    document.getElementById("birthdayMessage").textContent = "Surprise!";
+    document.getElementById("subMessage").textContent = "You are amazing, enjoy your day to the fullest!";
+}
